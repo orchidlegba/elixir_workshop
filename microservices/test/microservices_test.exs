966baca55d690f7da13a9c00c3f0545464a89c78
Frank Vierbergen <fvierbergen@mirabeau.nl>
2015-04-13 16:48:25 +0200

    tabs -> spaces

--- microservices/test/microservices_test.exs
+++ microservices/test/microservices_test.exs
@@ -2,7 +2,7 @@ defmodule MicroservicesTest do
   use ExUnit.Case
 
   test "find the last item in list with an Actor Microservice" do
-  	resultAnswer = RecursionActor.Client.find_last_item([1,2,3,4,5])  	 
+    resultAnswer = RecursionActor.Client.find_last_item([1,2,3,4,5])  	 
     assert resultAnswer == 5
   end
 end
